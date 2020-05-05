123a6224357b99431c0b69e291f5fde9a58cfefb
theMistletoe <katachiwotodomete@gmail.com>
2020-05-05 22:15:54 +0900

    update: feature on create new workspace

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="form-workspaceID">
-    <label>workspaceID</label>
-    <input type="text" v-model="workspaceID" />
-    <nuxt-link :to="`/workspace?workspaceID=${workspaceID}`">
-      Go to Next
-    </nuxt-link>
+  <div>
+    <button v-on:click="createWorkspace">Create new workspace!</button>
+    <div class="form-workspaceID">
+      <label>workspaceID</label>
+      <input type="text" v-model="workspaceID" />
+      <nuxt-link :to="`/workspace?workspaceID=${workspaceID}`">
+        Go to Next
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
+import shortid from "shortid";
+
 export default {
   data() {
     return {
       workspaceID: ""
     };
   },
-  methods: {}
+  methods: {
+    createWorkspace() {
+      const newWorkspaceID = shortid.generate();
+      this.$router.push(`/workspace?workspaceID=${newWorkspaceID}`);
+    }
+  }
 };
 </script>
 
