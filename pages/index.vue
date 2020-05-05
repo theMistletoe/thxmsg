cb8a9bb53c9bb2dc718e3359a76992f25558b503
theMistletoe <katachiwotodomete@gmail.com>
2020-05-05 22:48:26 +0900

    update: apply css index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
-    <button v-on:click="createWorkspace">Create new workspace!</button>
-    <div class="form-workspaceID">
-      <label>workspaceID</label>
-      <input type="text" v-model="workspaceID" />
-      <nuxt-link :to="`/workspace?workspaceID=${workspaceID}`">
-        Go to Next
-      </nuxt-link>
-    </div>
-  </div>
+  <main>
+    <header>
+      <h1>感謝の気持ちを伝えよう！</h1>
+    </header>
+    <section>
+      <aside>
+        <h2>新しいワークスペースを作る</h2>
+        <button v-on:click="createWorkspace">Create new workspace!</button>
+      </aside>
+    </section>
+    <section>
+      <aside>
+        <h2>どこかのワークスペースに参加する</h2>
+        <div class="form-workspaceID">
+          <label>workspaceID</label>
+          <input type="text" v-model="workspaceID" />
+          <nuxt-link :to="`/workspace?workspaceID=${workspaceID}`">
+            Go to workspace!
+          </nuxt-link>
+        </div>
+      </aside>
+    </section>
+  </main>
 </template>
 
 <script>
