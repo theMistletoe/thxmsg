a92d91c09428f55687822c4d82252356d2e35eb1
theMistletoe <katachiwotodomete@gmail.com>
2020-05-31 22:26:48 +0900

    update: use composition-api to WorkspaceID and mod test

--- components/WorkspaceID.vue
+++ components/WorkspaceID.vue
@@ -5,15 +5,35 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: ["workspaceID"],
-  methods: {
-    onCopy() {
-      this.$copyText(location.href);
-    }
-  }
+<script lang="ts">
+import { defineComponent, PropType, SetupContext } from "@vue/composition-api";
+
+type Props = {
+  workspaceID: string;
 };
+
+export default defineComponent({
+  props: {
+    workspaceID: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props: Props, context: SetupContext) {
+    const onCopy = () => {
+      navigator.clipboard
+        .writeText(location.href)
+        .then(() => {
+          console.log("テキストコピー完了");
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    };
+
+    return { onCopy };
+  }
+});
 </script>
 
 <style lang="css" scoped></style>
--- test/WorkspaceID.spec.ts
+++ test/WorkspaceID.spec.ts
@@ -1,12 +1,17 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import VueCompositionApi from "@vue/composition-api";
 import WorkspaceID from "~/components/WorkspaceID.vue";
+
+const localVue = createLocalVue();
+localVue.use(VueCompositionApi);
 
 describe("WorkspaceID.vue", () => {
   test("renders props.workspaceID when passed", () => {
     const workspaceID = "xxxxxxx";
     const wrapper = shallowMount(WorkspaceID, {
-      propsData: { workspaceID }
+      propsData: { workspaceID },
+      localVue
     });
-    expect(wrapper.text()).toMatch("workspaceID");
+    expect(wrapper.text()).toBe("WorkspaceID:xxxxxxx URL Copy!");
   });
 });
