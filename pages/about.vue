d94b1ce071ebdfb2d02e0c0595a11822450fcff0
theMistletoe <katachiwotodomete@gmail.com>
2020-06-20 00:02:11 +0900

    add: about page

---
+++ pages/about.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>このページは？</h1>
+      </header>
+      <article>
+        このページはアジャイルレトロスペクティブで紹介されている<br />
+        「感謝」というふりかえり手法をオンラインで行うための<br />
+        Webアプリです！
+      </article>
+    </div>
+
+    <div class="container">
+      <header>
+        <h1>感謝とは?</h1>
+      </header>
+      <article>
+        振り返り手法の一つ。<br />
+        チームメンバーがお互いに感謝の思いを伝え合うことで、<br />
+        ポジティブにイテレーションやふりかえりを終えられる効果があります！<br />
+        節目のタイミングや、チームが暗い雰囲気の際にチームを明るくできる効果があります！
+        <p>
+          参考：
+          <ul>
+            <li>
+              <i><a href="https://www.amazon.co.jp/dp/B01IGW59NQ/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1">
+              『アジャイルレトロスペクティブズ　強いチームを育てる「ふりかえり」の手引き』(EstherDerby, DianaLarsen他)</a></i>
+            </li>
+            <li>
+              <i><a href="http://management30.jp/2019/07/09/management30-action-marietta/">
+              『【やってみた】いい規模の会社で導入してみた結果・・・』</a></i>
+            </li>
+          </ul>
+        </p>
+      </article>
+    </div>
+  </main>
+</template>
+
+<script></script>
+
+<style lang="css">
+.container {
+  margin-left: 4em;
+  margin-right: 4em;
+}
+</style>
