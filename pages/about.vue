d93e40549387016e5d99beecffdfe771269452c4
Motoaki Tanaka <katachiwotodomete@gmail.com>
2023-06-04 12:29:14 +0000

    外部リンク

--- pages/about.vue
+++ pages/about.vue
@@ -22,15 +22,15 @@
         参考：
         <ul>
           <li>
-            <i><a href="https://www.amazon.co.jp/dp/B01IGW59NQ/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1">
+            <i><a href="https://www.amazon.co.jp/dp/B01IGW59NQ/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1" target="_blank" rel="noopener">
             『アジャイルレトロスペクティブズ　強いチームを育てる「ふりかえり」の手引き』(EstherDerby, DianaLarsen他)</a></i>
           </li>
           <li>
-            <i><a href="https://management30.com/practice/kudo-cards/">
+            <i><a href="https://management30.com/practice/kudo-cards/" target="_blank" rel="noopener">
             『Kudo Cards』(Management3.0)</a></i>
           </li>
           <li>
-            <i><a href="http://management30.jp/2019/07/09/management30-action-marietta/">
+            <i><a href="http://management30.jp/2019/07/09/management30-action-marietta/" target="_blank" rel="noopener">
             『【やってみた】いい規模の会社で導入してみた結果・・・』</a></i>
           </li>
         </ul>
